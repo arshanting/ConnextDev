2a72b1438a1ae31301245c117ba39ec2c94244a1
arshanting <arshanting@gmail.com>
2017-02-09 17:06:09 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Connext Global Solutions</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- <a class="navbar-brand" href="index.html">Start Bootstrap</a> -->
+                <a href="index.html"><img id="logo" src="./assets/home/connext_logo.png"></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.html">HOME</a>
+                    </li>
+                    <li>
+                        <a href="about.html">ABOUT</a>
+                    </li>
+                    <li>
+                        <a href="#">CAREERS</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <header class="intro-header" style="background-image: url('./assets/home/home_banner-img.png')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1>Lorem ipsum dolor sit amet,</h1>
+                        <h1>consectetur adipiscing elit.</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!--company services -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="abc-head">
+            <h3>Health Care Services</h3>
+          </div>
+          <div class="abc-image">
+            <img src="./assets/Home/home_services1-img.png">
+          </div>
+          <div class="abc-desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris sid alupe hes polo.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="abc-head">
+            <h3>Escrow Services</h3>
+          </div>
+          <div class="abc-image">
+            <img src="./assets/Home/home_services2-img.png">
+          </div>
+          <div class="abc-desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris sid alupe hes polo.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="abc-head">
+            <h3>Legal Services</h3>
+          </div>
+          <div class="abc-image">
+            <img src="./assets/Home/home_services3-img.png">
+          </div>
+          <div class="abc-desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris sid alupe hes polo.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="abc-head">
+            <h3>Other Services</h3>
+          </div>
+          <div class="abc-image">
+            <img src="./assets/Home/home_services4-img.png">
+          </div>
+          <div class="abc-desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris sid alupe hes polo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--careers -->
+    <div class="container">
+      <div class="row">
+        <div id="temp">
+          <div class="col-md-6 col-sm-12 col-xs-12">
+            <div class="cr-image image-left">
+              <img src="./assets/Home/go-to_careers-img.png">
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-12 col-xs-12">
+            <div class="cr-head">
+              <h3>Looking for an exciting new career?</h3>
+            </div>
+            <div class="cr-desc">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+            <div class="cr-button">
+              <input type="button" class="go-to-careers"value="GO TO CAREERS">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr></hr>
+
+    <div class="container">
+      <div class="row">
+        <div class="cs-studies-head">
+          <h2>Our Case Studies<h2>
+        </div>
+      </div>
+    </div>
+
+    <!--case studies -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="cs-head">
+            <p>02 FEB 2016<p>
+            <h3>Support Services a success.</h3>
+          </div>
+          <div class="cs-desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim...
+            </p>
+          </div>
+          <div class="read-more">
+            <a href="#">READ MORE →</a>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="cs-head">
+            <p>12 FEB 2016<p>
+            <h3>Support Services a success.</h3>
+          </div>
+          <div class="cs-desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim...
+            </p>
+          </div>
+          <div class="read-more">
+            <a href="#">READ MORE →</a>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="cs-head">
+            <p>29 FEB 2016<p>
+            <h3>Support Services a success.</h3>
+          </div>
+          <div class="cs-desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim...
+            </p>
+          </div>
+          <div class="read-more">
+            <a href="#">READ MORE →</a>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="cs-head">
+            <p>05 FEB 2016<p>
+            <h3>Support Services a success.</h3>
+          </div>
+          <div class="cs-desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim...
+            </p>
+          </div>
+          <div class="read-more">
+            <a href="#">READ MORE →</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- footer-->
+    <div class="footer-bg-color">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <div class="location">
+              <p>Unit 1 FG Bldg 9, Philexcel Business Center, Clarkfield, Pampanga
+                Philippines 2023</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="location">
+              <ul class="list-inline">
+                <li>
+                    <a href="#">
+                        <span class="fa-stack fa-lg">
+                            <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                      <span class="fa-stack fa-lg">
+                          <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="fa-stack fa-lg">
+                            <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    <div class="container">
+      <div class="hr-footer">
+          <hr></hr>
+      </div>
+        <div class="container">
+          <div class="copyright">
+            <p>&copy 2016 Connext LLC. All rights reserved<p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
